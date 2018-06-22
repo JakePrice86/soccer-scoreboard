cd3a89e50a464347975cae02d9e5f58a724fe6e4
Jake Price <jp@jakelprice.com>
2018-06-22 16:27:43 +0100

    Recording now done in sync with live

--- var.js
+++ var.js
@@ -1,6 +1,7 @@
 window.VAR = {
 
-    video: {},
+    recorder: {},
+    videoChunks: [],
     keys: {
         home: "h",
         away: "a",
@@ -15,14 +16,20 @@ window.VAR = {
     time: 0,
     home: 0,
     away: 0,
-    buffer: 10000,
+    tick: 100,
     replay: false,
+    teams: {
+        home: 'England',
+        away: 'Spain'
+    },
 
     init: function ()
     {
         this.newCamera()
         this.initKnob()
         $('body').keydown(key => this.buttonPress(key))
+        $('.team-home-name').html(this.teams.home)
+        $('.team-away-name').html(this.teams.away)
     },
 
     camera: function ()
@@ -39,80 +46,105 @@ window.VAR = {
 
     newCamera: function ()
     {
-
         if (this.replay) { return true }
+
+        this.recorder = {}
+        this.videoChunks = []
+
         let video = document.getElementById('video');
         let recording = document.getElementById("recording");
+
+        console.log('Starting recording')
 
         navigator.mediaDevices.getUserMedia({
             video: true,
             audio: true
         }).then(stream => {
             video.srcObject = stream;
-            // downloadButton.href = stream;
             video.captureStream = video.captureStream || video.mozCaptureStream;
             return new Promise(resolve => video.onplaying = resolve);
         }).then(() => this.startRecording(
-            video.captureStream(), this.buffer)
+            video.captureStream())
         )
-        .then(recordedChunks => {
-
-            //-- Once we have recorded, lets restart
-            let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-            recording.src = URL.createObjectURL(recordedBlob);
-            this.newCamera()
-        })
         .catch(e => {
             console.log(e)
         });
     },
 
-    startRecording: function (stream, lengthInMS)
+    startRecording: function (stream)
     {
-        let recorder = new MediaRecorder(stream);
+        this.recorder = new MediaRecorder(stream);
         let data = [];
-
-        recorder.ondataavailable = event => data.push(event.data);
-        recorder.start();
-        console.log(recorder.state + " for " + (lengthInMS / 1000) + " seconds...");
+        
+        this.recorder.ondataavailable = event => data.push(event.data);
+        this.recorder.start();
 
         let stopped = new Promise((resolve, reject) => {
-            recorder.onstop = resolve;
-            recorder.onerror = event => reject(event.name);
+            this.recorder.onstop = resolve;
+            this.recorder.onerror = event => reject(event.name);
         });
 
-        let recorded = this.wait(lengthInMS).then(
-            () => recorder.state == "recording" && recorder.stop()
-        );
+        this.recorder.ondataavailable = (e => {
+            this.createReplayVideo(e)
+        })
 
-        return Promise.all([
-            stopped,
-            recorded
-        ])
-        .then(() => data);
+        return true
+    },
 
+    testReplay: function ()
+    {
+        this.recorder.requestData(blob => {
+            console.log(blob)
+        })
     },
 
     showReplay: function ()
     { 
 
         this.replay = true
-        let video = document.getElementById('video');
-        video.style.display = "none"
 
         $('.replay').show()
 
         let recording = document.getElementById("recording");
         recording.style.display = "block"
-        recording.play()
+        
+        //-- Request our data
+        this.recorder.requestData();
+        
+    },
+
+    createReplayVideo: function (e)
+    {
+        this.videoChunks.push(e.data)
+        let recordedBlob = new Blob(this.videoChunks, { type: "video/webm" });
+        let recording = document.getElementById("recording");
+        recording.src = URL.createObjectURL(recordedBlob);
+        
+
+        recording.addEventListener('durationchange', () => {
+            recording.currentTime = 999999
+            if (recording.readyState) {
+                let time = parseInt(recording.duration, 10);
+                if (time > 0)
+                {
+                    console.log('duration loaded', time)
+                    recording.currentTime = 0
+                    //-- process
+                    if (time > 10) {
+                        recording.currentTime = time - 10
+                    }
+                }
+            }
+        });
+
+        //-- Stop recording
+        this.recorder.stops
         
     },
 
     stopReplay: function ()
     {
         this.replay = false
-        let video = document.getElementById('video');
-        video.style.display = "block"
 
         $('.replay').hide()
 
@@ -171,16 +203,18 @@ window.VAR = {
     {
         this.timeout = setInterval(() => {
             this.updateTimer()
-        }, 5000)
+        }, this.tick)
     },
 
     updateTimer: function () 
     {
+        if (this.replay) { return true }
+
+        //-- Check for times
         if (this.time == 45)
         {
             //-- STOP
             clearInterval(this.timeout)
-            console.log('stop')
         }
         else
         {
